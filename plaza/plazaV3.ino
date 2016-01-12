4cee0811fadf1fa17b4f9d62faf89b94456f90bd
jorge-canas <sergejorge@gmail.com>
2016-01-12 13:54:07 +0100

    Update plazaV3.ino
    Adaptado a la fusión fuzzy

--- plaza/plazaV3.ino
+++ plaza/plazaV3.ino
@@ -40,38 +40,39 @@ long tiempo;
 //definicion de las constantes utilizadas
 const int echo=6; //pin de arduino donde se conecta echo
 const int trigger=7;// pin de arduino donde se conecta trigger
-const int offsetultra=2; // offset calculado de forma manual 
+const int offsetUltra=2; // offset calculado de forma manual 
 const int ledVerde=13;// pin de arduino donde se conecta el led verde
 const int ledRojo=12;// pin de arduino donde se conecta el led rojo
-const int NMEDIDAS=10; //numero de medidas para la calibracion
-const int NMODAS=400;//numero de medidas posibles en la calibracion
-const int maxmediamovil=190;//valor maximo correcto para el ultrasonido (media movil) //modificado
-const int minmediamovil=70; //valor minimo correcto para el ultrasonido (media movil) //modificado
-const int tolmediamovil=20;//valor de tolerancia para ultrasonido (media movil) //modificado
+const int N_MEDIDAS_CAL=10; //numero de medidas para la calibracion
+const int N_MEDIDAS_USO=5; //numero de medidas para la calibracion
+const int N_MODAS=400;//numero de medidas posibles en la calibracion
+const int maxMediaMovil=190;//valor maximo correcto para el ultrasonido (media movil) //modificado
+const int minMediaMovil=70; //valor minimo correcto para el ultrasonido (media movil) //modificado
+const int tolMediaMovil=20;//valor de tolerancia para ultrasonido (media movil) //modificado
 
-const int ESCENARIOSCALIBRAR=3; //numero de escenarios para calibrar
-const char escenarios [ESCENARIOSCALIBRAR][20]={"VACIO","ALTO","BAJO"}; //nombre de los escenarios a calibrar
-int mediasfoto[ESCENARIOSCALIBRAR];
-int mediasultra[ESCENARIOSCALIBRAR];
+const int ESCENARIOS_CALIBRAR=3; //numero de escenarios para calibrar
+const char escenarios [ESCENARIOS_CALIBRAR][6]={"VACIO","ALTO","BAJO"}; //nombre de los escenarios a calibrar
+int mediasFoto[ESCENARIOS_CALIBRAR];
+int mediasUltra[ESCENARIOS_CALIBRAR];
 
 // estructuras para la media movil para ultrasonido
-int mediamovilultra=0;
-int nmediamovilultra=0;
+int mediaMovilUltra=0;
+int nMediaMovilUltra=0;
 
-int medidas [NMEDIDAS];//arreglo para calcular media y mediana
+int medidas [N_MEDIDAS_CAL];//arreglo para calcular media y mediana
 int media; 
-int valoresmoda[NMODAS]; //ya que los valores se encuentran entre 2 y 400, cada vez que se mida se inserta en este arreglo y al final en el procesamiento, se comprueba la moda.
+int valoresModa[N_MODAS]; //ya que los valores se encuentran entre 2 y 400, cada vez que se mida se inserta en este arreglo y al final en el procesamiento, se comprueba la moda.
 int moda;
 
 //Para el LDR
 const int foto=A0;//pin para la medicion del LDR
-const int fotooffset=40; //offset calculado experimentalmente para el LDR
-const int NUMVALORESFOTO=8;
+const int fotoOffset=40; //offset calculado experimentalmente para el LDR
+const int NUM_VALORES_FOTO=8;
 
 int valorfoto=0;
 
 //creacion de la tabla look-up insertando los valores experimentales
-int tablalu[NUMVALORESFOTO][2]={{306,423},{362,473},{471,491},{578,595},{647,611},{1033,656},{1303,699},{1343,719}};
+int tablaLumen[NUM_VALORES_FOTO][2]={{306,423},{362,473},{471,491},{578,595},{647,611},{1033,656},{1303,699},{1343,719}};
 
 //Para el almacenamiento en EEPROM
 //Se intenta dejar espacio entre los elementos a almacenar para guardar más información en un futuro
@@ -87,7 +88,6 @@ const int dirLDR = 10;
 const int dirUltra = 50;
 
 void setup(){
-
   //Iniciar fuzzy
   initFuzzy();
   
@@ -105,12 +105,12 @@ void setup(){
   calibrado = loadEEPROM(dirCalibrado);
   if(calibrado == 's'){
     Serial.println("Recuperando calibracion");
-    for(int i=0;i<ESCENARIOSCALIBRAR;i++)
+    for(int i=0;i<ESCENARIOS_CALIBRAR;i++)
      {
        Serial.println("calibrando LDR");
-       mediasfoto[i] = loadEEPROM(dirLDR + i*4); //modificado
+       mediasFoto[i] = loadEEPROM(dirLDR + i*4); //modificado
        Serial.println("calibrando ULTRA");;
-       mediasultra[i] = loadEEPROM(dirUltra + i*4); //modificado
+       mediasUltra[i] = loadEEPROM(dirUltra + i*4); //modificado
      }
   }
 
@@ -121,109 +121,77 @@ void setup(){
 void loop(){
   /*for(int i=0;i<8;i++){
   for(int j=0;j<2;j++){
-    Serial.print(tablalu[i][j]);
+    Serial.print(tablaLumen[i][j]);
     Serial.print("---");
   }
   Serial.println();
-}*/
-  
+}*/  
   int mediafoto;
   int mediaultra;
   int opcion;
   
-  /*while(opcion!=1 && !calibrado)
-  {
+  /*while(opcion!=1 && !calibrado){
     //MENSAJE DE QUE NECESITA CALIBRARSE PRIMERO
-    
   }*/
   Serial.println("INICIO");
   opcion=Serial.read();
 
-  switch(opcion)
-  {    
+  switch(opcion){
     case '1':
-     for(int i=0;i<ESCENARIOSCALIBRAR;i++)
-     {
-         while (Serial.read() !='0') {
-             Serial.println("INTRODUZCA 0 PARA CONTINUAR");
-         }
-       Serial.println(escenarios[i]);
-       
-       
-       digitalWrite(ledRojo,HIGH);
-       digitalWrite(ledVerde,LOW);
-       Serial.println("calibrando LDR");
-       mediasfoto[i]=calibraLDR();
-       saveEEPROM(mediasfoto[i], dirLDR+ i*4); //modificado
-       Serial.println("calibrando ULTRA");
-       mediasultra[i]=calibraUltrasonico();
-       saveEEPROM(mediasultra[i], dirUltra+ i*4); //modificado
-       digitalWrite(ledRojo,LOW);
-       digitalWrite(ledVerde,HIGH);
-     }
-     
-     calibrado='s'; //modificado
-     saveEEPROM(calibrado, dirCalibrado);
-     
+      calibrar();
     break;
   }
+
+  int coche = getFuzzyValue(medidaLDR(N_MEDIDAS_USO), medidaUltrasonico(N_MEDIDAS_USO));
   
   delay(500);
 }
 
 int valorLookUp(int entrada){
   int resultado=0;
-  
   int valorfiltrado=0;
   
-  if(entrada<tablalu[0][1] || entrada>tablalu[NUMVALORESFOTO-1][1]){
-    
-    entrada=constrain(entrada,tablalu[0][1],tablalu[NUMVALORESFOTO-1][1]);
-    if(entrada==tablalu[0][1]){
-      resultado=tablalu[0][0];
+  if(entrada<tablaLumen[0][1] || entrada>tablaLumen[NUM_VALORES_FOTO-1][1]){
+    entrada=constrain(entrada,tablaLumen[0][1],tablaLumen[NUM_VALORES_FOTO-1][1]);
+    if(entrada==tablaLumen[0][1]){
+      resultado=tablaLumen[0][0];
     }else{
-       resultado= tablalu[NUMVALORESFOTO-1][0];
+       resultado= tablaLumen[NUM_VALORES_FOTO-1][0];
     }
   }else{
-    
-     //La calibracion se realiza a traves de una regresion por cuadrados minimos.
-      int i=0;
-      boolean encontrado=false;
-      while(i+1<NUMVALORESFOTO && !encontrado){
+    //La calibracion se realiza a traves de una regresion por cuadrados minimos.
+    int i=0;
+    boolean encontrado=false;
+    while(i+1<NUM_VALORES_FOTO && !encontrado){
+      if(i+1>=entrada){
         
-        if(i+1>=entrada)
-        {
-          
-          int x=entrada;
-          int x0=tablalu[i][1];
-          int x1=tablalu[i+1][1];
-          int y0=tablalu[i][0];
-          int y1=tablalu[i+1][0];
-          encontrado=true;
-          resultado= y0+(((x-x0)/(x1-x0))*(y1-y0));
-        }
-        i++;
-        
-        
-        
+        int x=entrada;
+        int x0=tablaLumen[i][1];
+        int x1=tablaLumen[i+1][1];
+        int y0=tablaLumen[i][0];
+        int y1=tablaLumen[i+1][0];
+        encontrado=true;
+        resultado= y0+(((x-x0)/(x1-x0))*(y1-y0));
       }
+      i++;
+    }
   }
     
   return resultado;
 }
 
-int calibraLDR(){
+int medidaLDR(int repeticiones){
   
   //inicializacion de media,mediana y moda al comenzar el ciclo de medicion.
   media=0;
   //moda=0;
 
-  for(int i=0;i<NMEDIDAS;i++){
+  for(int i=0;i<repeticiones;i++){
     medidas[i]=0;
   }
   //comienzo del ciclo de medicion
   
-  for(int i=0;i<NMEDIDAS;i++){
+  for(int i=0;i<repeticiones;i++){
    // Serial.print("es la vez: ");
    // Serial.println(i+1);
       
@@ -237,62 +205,58 @@ int calibraLDR(){
       media+=valor;
     
       //insertamos de forma ordenada
-        int j=0;
-        int k;
-  
-        while( j<i && valor<medidas[j] ){
-            j++;  
-         };
-         
-          if(j==i){
-            medidas[j]=valor;
-            
-          }else{
-            k=i;
-            
-            while(k>=j){
-              medidas[k]=medidas[k-1];
-              k--;
-            }
-            medidas[j]=valor;
+      int j=0;
+      int k;
+
+      while( j<i && valor<medidas[j] ){
+          j++;  
+       };
+       
+      if(j==i){
+        medidas[j]=valor;
+        
+      }else{
+        k=i;
+        
+        while(k>=j){
+          medidas[k]=medidas[k-1];
+          k--;
         }
+        medidas[j]=valor;
+      }
     delay(5000);
   }
 
-    /*for(int i=0;i<NMEDIDAS;i++){
+    /*for(int i=0;i<repeticiones;i++){
       Serial.print(medidas[i]);
        Serial.print("-");
     }  */
     Serial.println();
     Serial.print("MEDIANA: distancia ");
-    Serial.print(medidas[NMEDIDAS/2]);
+    Serial.print(medidas[repeticiones/2]);
     Serial.println(" lux");
     Serial.print("MEDIA: distancia ");
    // Serial.print(media);
-   int resultado=media/NMEDIDAS;
+   int resultado=media/repeticiones;
     Serial.print(resultado);
     Serial.println(" lux");
     
     return resultado;
 }
 
-int calibraUltrasonico(){
+int medidaUltrasonico(int repeticiones){
    //inicializacion de media,mediana y moda al comenzar el ciclo de medicion.
   media=0;
   //moda=0;
-  for(int i=0;i<NMODAS;i++)
-  {
-    valoresmoda[i]=0;
+  for(int i=0;i<N_MODAS;i++){
+    valoresModa[i]=0;
   }
-  for(int i=0;i<NMEDIDAS;i++)
-  {
+  for(int i=0;i<repeticiones;i++){
     medidas[i]=0;
   }
   
   //comienzo del ciclo de medicion
-  
-  for(int i=0;i<NMEDIDAS;i++)
-  {
+  for(int i=0;i<repeticiones;i++){
       digitalWrite(trigger,LOW); /* Por cuestión de estabilización del sensor*/
       delayMicroseconds(10);// ya que tarda segun las especificaciones unos 10us en pasar de un estado al otro
       digitalWrite(trigger, HIGH); /* envío del pulso ultrasónico*/
@@ -300,51 +264,43 @@ int calibraUltrasonico(){
       tiempo=pulseIn(echo, HIGH); /* Función para medir la longitud del pulso entrante. Mide el tiempo que transcurrido entre el envío
       del pulso ultrasónico y cuando el sensor recibe el rebote, es decir: desde que el pin 12 empieza a recibir el rebote, HIGH, hasta que
       deja de hacerlo, LOW, la longitud del pulso entrante*/
-      distancia= 10*((int(tiempo/58))+offsetultra);//distancia= int(0.017*tiempo); /*fórmula para calcular la distancia obteniendo un valor entero se trata de una aproximacin de us/58*/ //modificado
+      distancia= 10*((int(tiempo/58))+offsetUltra);//distancia= int(0.017*tiempo); /*fórmula para calcular la distancia obteniendo un valor entero se trata de una aproximacin de us/58*/ //modificado
       //definimos que se encuentre dentro del rango establecido
       distancia=constrain(distancia,30,4000); //modificado
       //vamos preparando la media
       //Serial.println(distancia);
       media+=distancia;
       //Se adquieren los datos para una media movil 
-      if(minmediamovil-tolmediamovil>distancia && distancia<maxmediamovil+tolmediamovil){
-          mediamovilultra=mediamovilultra+distancia;
-          nmediamovilultra=nmediamovilultra+1;
+      if(minMediaMovil-tolMediaMovil>distancia && distancia<maxMediaMovil+tolMediaMovil){
+          mediaMovilUltra=mediaMovilUltra+distancia;
+          nMediaMovilUltra=nMediaMovilUltra+1;
       }
-      int mediamovilultra[ESCENARIOSCALIBRAR];
+      int mediaMovilUltra[ESCENARIOS_CALIBRAR];
 
       //preparamos la moda
-      valoresmoda[distancia]++;
+      valoresModa[distancia]++;
       //insertamos de forma ordenada
-        
-        int j=0;
-        int k;
-  
-        while( j<i && distancia<medidas[j] ){
-            j++;  
-         };
-         
-          if(j==i){
-            medidas[j]=distancia;
-            
-          }else{
-            k=i;
-            
-            while(k>=j){
-              medidas[k]=medidas[k-1];
-              k--;
-            }
-            medidas[j]=distancia;
-        
-          
-        }
-    delay(50);
-      
-  }
+      int j=0;
+      int k;
+      while( j<i && distancia<medidas[j] ){
+        j++;
+      }
+      if(j==i){
+        medidas[j]=distancia;
+      }else{
+          k=i;
+          while(k>=j){
+            medidas[k]=medidas[k-1];
+            k--;
+          }
+          medidas[j]=distancia;
+      }
+      delay(50);
+    }
     moda=0;
-    valoresmoda[0]=1;
-    for(int i=0;i<NMODAS;i++){
-      if(valoresmoda[i]>moda){
+    valoresModa[0]=1;
+    for(int i=0;i<N_MODAS;i++){
+      if(valoresModa[i]>moda){
         moda=i;
       }
     }
@@ -352,17 +308,17 @@ int calibraUltrasonico(){
  
     Serial.println();
     Serial.print("MEDIANA: distancia ");
-    Serial.print(medidas[NMEDIDAS/2]);
-    resultado = medidas[NMEDIDAS/2]; //modificado
+    Serial.print(medidas[repeticiones/2]);
+    resultado = medidas[repeticiones/2]; //modificado
     Serial.println(" cm");
     Serial.print("MEDIA: distancia ");
    // Serial.print(media);
-    //resultado=media/NMEDIDAS; //modificado
+    //resultado=media/repeticiones; //modificado
     Serial.print(resultado);
     Serial.println(" cm");
     Serial.print("MEDIA MOVIL: distancia ");
    // Serial.print(media);
-    //resultado=mediamovilultra/nmediamovilultra; //modificado
+    //resultado=mediaMovilUltra/nMediaMovilUltra; //modificado
     Serial.print(resultado);
     Serial.println(" cm");
     Serial.print("MODA: distancia ");
@@ -372,50 +328,51 @@ int calibraUltrasonico(){
     return resultado;
 }
 
-
 //Metodo receiveEvent
 
-void receiveEvent(int opt)
-{
+void receiveEvent(int opt){
   Serial.print("al menos llega");
-  while(0<Wire.available())
-  {
+  while(0<Wire.available()){
     char c=Wire.read();//con el read se coge 1 B, otro metodo para leeer X B de golpe
     Serial.print("recibo a traves de I2C: ");
     Serial.println(c);
     if(c=='c'){
-     
-      for(int i=0;i<ESCENARIOSCALIBRAR;i++)
-     {
-       Serial.print("escenario: ");
-       Serial.println(i);
-       c='0';
-         Serial.print("*********************************************************");
-       Serial.print("termina el ");
-       Serial.println(i);
-       //probar con ack/currentmillis 
-       delay(15000);
-      /* Wire.write("ok");
-       char c=Wire.read();
-        while (c !='c') {
-          char c=Wire.read();
-         }*/
-       Serial.println(escenarios[i]);
-       
-       
-       digitalWrite(ledRojo,HIGH);
-        digitalWrite(ledVerde,LOW);
-       Serial.println("calibrando LDR");
-       mediasfoto[i]=calibraLDR();
-       Serial.println("calibrando ULTRA");
-       mediasultra[i]=calibraUltrasonico();  
-       digitalWrite(ledRojo,LOW);
-       digitalWrite(ledVerde,HIGH);
-       
-     }
-      
+      calibrar();
     }
-  }  
+  }
+}
+
+/////////////////////////////////////////////// CALIBRAR ///////////////////////////////////////////////
+void calibrar(){
+  for(int i=0;i<ESCENARIOS_CALIBRAR;i++){
+     Serial.print("escenario: ");
+     Serial.println(i);
+     int c='0';
+     Serial.print("*********************************************************");
+     Serial.print("termina el ");
+     Serial.println(i);
+     //probar con ack/currentmillis 
+     delay(15000);
+    /* Wire.write("ok");
+     char c=Wire.read();
+      while (c !='c') {
+        char c=Wire.read();
+       }*/
+     Serial.println(escenarios[i]);
+     
+     digitalWrite(ledRojo,HIGH);
+     digitalWrite(ledVerde,LOW);
+     Serial.println("calibrando LDR");
+     mediasFoto[i]=medidaLDR(N_MEDIDAS_USO);
+     saveEEPROM(mediasFoto[i], dirLDR+ i*4); //modificado
+     Serial.println("calibrando ULTRA");
+     mediasUltra[i]=medidaUltrasonico(N_MEDIDAS_USO);  
+     saveEEPROM(mediasUltra[i], dirUltra+ i*4); //modificado
+     digitalWrite(ledRojo,LOW);
+     digitalWrite(ledVerde,HIGH);
+  }
+  calibrado='s'; //modificado
+  saveEEPROM(calibrado, dirCalibrado);
 }
 
 /////////////////////////////////////////////////////////////// Memoria EEPROM ///////////////////////////
@@ -480,7 +437,6 @@ void initFuzzy(){
  fuzzy->addFuzzyOutput(coche); // Añadir el conjunto de salida al objeto fuzzy
  
  //Paso 5 - Conjunto de reglas
-
  //Consecuentes
  FuzzyRuleConsequent* thenExisteCoche = new FuzzyRuleConsequent(); //Creación de consecuente
  thenExisteCoche->addOutput(existeCoche);// Juntando consecuente con salida
@@ -562,6 +518,3 @@ int getFuzzyValue(float luz, float distancia){
 
   return coche;
 }
-
-
-
